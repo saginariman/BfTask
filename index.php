78cbf0761337356d337db65285cbdd3ee523bd92
saginariman <sagiden8@gmail.com>
2021-06-30 09:57:58 +0600

    29.06.2021

--- index.php
+++ index.php
@@ -18,20 +18,20 @@
 </head>
 <body>
 	<div class="container-fluid p-5">
-		<h1>Форма Регистрации</h1>
+		<h1>Форма Регистрации ECP</h1>
 		<form method="post" id="formSignIn" enctype="multipart/form-data">
-			<input type="text" class="form-control mt-2" id="name" name="name" placeholder="Введите ФИО"><br>
+			<!-- <input type="text" class="form-control mt-2" id="name" name="name" placeholder="Введите ФИО"><br>
 			<input type="text" class="form-control mt-2" id="year" name="year" placeholder="Введите Дату Рождения"><br>
 			<input type="text" class="form-control mt-2" id="doc" name="doc" placeholder="Введите ИИН"><br>
 			<input type="text" class="form-control mt-2" id="tel" name="tel" placeholder="Введите номер телефона"><br>
-			<input type="text" class="form-control mt-2" id="adres" name="adres" placeholder="Введите Адрес"><br>
+			<input type="text" class="form-control mt-2" id="adres" name="adres" placeholder="Введите Адрес"><br> -->
 			<input type="file" class="form-control mt-2" id="file" name="file" /><br>
 			<input id="button" class="btn btn-success mt-3 btn-block" type="button" value="Отправить" />
 		</form>
 	</div>	
 	<div id="resultDiv" class="container-fluid text-success"></div><br>
 
-	<div class="container-fluid">
+	<!-- <div class="container-fluid">
 		<table class="table">
 		  <thead class="thead-inverse">
 		    <tr>
@@ -64,9 +64,9 @@
 		      <td>+77053259658</td>
 		      <td>New Yourk, street Beverly, 54</td>
 		      <td>/uploads/file_6006b968d5d7b7.19098174.jpg</td>
-		    </tr>
+		    </tr> -->
 
-		   <?php
+		   <!-- <?php
 		    	$mysqli=new mysqli("localhost","f0497377","ivkivieric","f0497377_nari");
 		    	$result=$mysqli->query("SELECT * FROM `files`");
 		    	$mysqli->close();
@@ -85,10 +85,10 @@
 						      <td>'.$array[$i]["path"].'</td>
 						   </tr>';
 		    	}
-		   ?>
-		  </tbody>
+		   ?> -->
+		  <!-- </tbody>
 		</table>
-	</div>	
+	</div>	 -->
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script  src="ajax.js"></script>
 </body>
